2cd7a3757a8ee8495eb35204260d250decdb5a66
Ray Perry <raymondscottperry@gmail.com>
2025-11-25 06:45:52 -0600

    fix cors blocking in cloud run

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,4 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
+const API_URL = import.meta.env.VITE_API_URL !== undefined 
+  ? import.meta.env.VITE_API_URL 
+  : 'http://localhost:4000';
 
 export const config = {
   apiUrl: API_URL,
